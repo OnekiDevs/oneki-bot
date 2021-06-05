5b221a9a734dcf9360d48b4f9734e28b15b88387
Elí Maciel <tecno_eli@hotmail.com>
2021-06-04 21:08:06 -0500

    args fixed

--- js/events/message.js
+++ js/events/message.js
@@ -6,7 +6,7 @@ module.exports = {
             if (message.channel.type == 'dm') return client.emit('directMessage', message);
             if (!message.content.toLowerCase().startsWith(client.settings.prefix)) return;
             const args = message.content.slice(client.settings.prefix.length).trim().split(/ +/g);
-            return client.emit('command', args.shift().toLowerCase(), message, args.slice(1));
+            return client.emit('command', args.shift().toLowerCase(), message, args);
         } catch (error) {
             client.emit('error', client, error, `Channel: <#${message.channel.id}>\nServer: ${message.guild.name} / ${message.guild.id}${message.content}`)
         }
