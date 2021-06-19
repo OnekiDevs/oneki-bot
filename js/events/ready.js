5db75f8facd27dc4c46d47d22886879310aa1b88
Elí Maciel <tecno_eli@hotmail.com>
2021-06-18 20:59:12 -0500

    cambio de db en prefix

--- js/events/ready.js
+++ js/events/ready.js
@@ -2,12 +2,19 @@ const db = require('firebase-admin').firestore();
 module.exports = {
     name: 'ready',
     run: async (client) => {
+        const config = await db.collection('config').get();
+        if (!config.empty) {
+            config.forEach(c => {
+                client.servers.set(c.id, {
+                    prefix: c.data()?.prefix ?? '>'
+                });
+            });
+        }
         client.guilds.cache.map(async guild => {
-            const config = await db.collection(guild.id).doc('config').get();
-            client.servers.set(guild.id, {
-                prefix: config.data()?.prefix ?? '>'
-            })
-        })
+            if (!client.servers.get(guild.id)) client.servers.set(guild.id, {
+                prefix: '>'
+            });
+        });
         console.log('\x1b[31m%s\x1b[0m', `${client.user.username} ${require('../../package.json').version} Listo y Atento!!!`);
     }
 }
