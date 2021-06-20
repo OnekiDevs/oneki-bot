16e00f78c784c7f5d402d7e85183dadb756cacdd
Elí Maciel <tecno_eli@hotmail.com>
2021-06-19 22:54:29 -0500

    update dependencies

---
+++ js/modules/uno/cartas.js
@@ -0,0 +1,21 @@
+const canvas = require('canvas');
+const fs = require('fs');   
+module.exports = async (maso) => {
+    return new Promise(async (resolve, reject) => {
+        let img = await canvas.loadImage(require('../../../src/unoCards.json')['3r'].url)
+        const c = canvas.createCanvas(((img.width*maso.length)/2)+(img.width/2), img.height);
+        const ctx = c.getContext('2d')
+        let p = 0
+        console.log(img.width);
+        for (const i of maso) {
+            img = await canvas.loadImage(require('../../../src/unoCards.json')[i].url)
+            await ctx.drawImage(img, (p*img.width)/2, 0, img.width, img.height)
+            console.log(p*img.width);
+            p++
+        }
+        fs.writeFile('prueba.png', c.toBuffer('image/png'), err => {
+            console.log(err);
+        })
+        resolve(c.toBuffer('image/png'));
+    })
+}
--- js/modules/uno/mostrar.js
+++ js/modules/uno/mostrar.js
@@ -1,50 +1,74 @@
-const canvas = require('canvas');
 const { MessageAttachment } = require('discord.js');
-const fs = require('fs');
+const fetch = require("node-fetch");
 module.exports = async (client, interact) => {
-    let partida = client.uno.get(interact.data.custom_id.slice(6))
-    const cartas = require('../../../src/unoCards.json');
-    console.log(partida[interact.member.user.id].cartas);
-    let img = await canvas.loadImage(cartas['3r'].url);
-    const c = canvas.createCanvas(((img.width*cartas.length)/2)+(img.width/2), img.height);
-    const ctx = c.getContext('2d')
-    let p = 0;
-    for (const i of partida[interact.member.user.id].cartas) {
-        console.log(i);
-        img = await canvas.loadImage(cartas[i].url)
-        await ctx.drawImage(img, (p*img.width)/2, 0, img.width, img.height);
-        p++
-    }
-    const buff = await c.toBuffer('image/png', {
-        compressionLevel: 6,
-        filters: canvas.PNG_ALL_FILTERS
-    });
-    console.log(buff);
-    const attachment = new MessageAttachment(buff);
-    console.log(attachment);
+    let partida = client.uno.get(interact.data.custom_id.slice(6));
     if (partida) {
         client.api.interactions(interact.id, interact.token).callback.post({
             data: {
-                type: 4,
-                data: {
-                    components: [
-                        {
-                            type: 1, 
-                            components: [
-                                {
-                                    type: 2,
-                                    style: 2,
-                                    custom_id: `uno_j_rg_${partida.id}`,
-                                    label: "rg"
-                                }
-                            ]
-                        }
-                    ],
-                    flags: 1 << 6,
-                    files: [attachment]
-                }
+                type: 5
             }
+        })
+        const maso = await require('./cartas')(partida[interact.member.user.id].cartas);
+        const attachment = new MessageAttachment(maso);
+
+        const msg = await client.api.webhooks(client.user.id, interact.token).messages('@original').get();
+        console.log(msg);
+        fetch(`https://discord.com/api/v9/webhooks/${client.user.id}/${interact.token}/messages/@original`, { 
+            method: 'PATCH',
+            headers: { 
+                'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify({
+                data: {
+                    type: 4,
+                    data: {
+                        content: 'prueba',
+                        components: [
+                            {
+                                type: 1, 
+                                components: [
+                                    {
+                                        type: 2,
+                                        style: 2,
+                                        custom_id: `uno_j_rg_${partida.id}`,
+                                        label: "rg"
+                                    }
+                                ]
+                            }
+                        ],
+                        flags: 1 << 6,
+                        files: [{ attachment: maso, name: "cartas.png" }]
+                    }
+                }
+            })
+        }).then(res => res.json())
+        .then(json => {
+            console.log(json)
+            console.log(json.errors)
         });
+        // client.api.webhooks(client.user.id, interact.token).messages('@original').patch({
+        //     data: {
+        //         type: 4,
+        //         data: {
+        //             content: 'prueba',
+        //             components: [
+        //                 {
+        //                     type: 1, 
+        //                     components: [
+        //                         {
+        //                             type: 2,
+        //                             style: 2,
+        //                             custom_id: `uno_j_rg_${partida.id}`,
+        //                             label: "rg"
+        //                         }
+        //                     ]
+        //                 }
+        //             ],
+        //             flags: 1 << 6,
+        //             files: [{ attachment: maso, name: "cartas.png" }]
+        //         }
+        //     }
+        // });
     } else {
         client.api.interactions(interact.id, interact.token).callback.post({
             data: {
