54a6a870a05224dde25b751792229fce5d587381
Elí Maciel <tecno_eli@hotmail.com>
2021-06-21 23:48:21 -0500

    message.guild.me.hasPermission is not a function fixed

--- js/events/command.js
+++ js/events/command.js
@@ -2,11 +2,11 @@ module.exports = {
     name: 'command',
     run: async (client, command, message, args) => {
         try {
-            if(!message.guild.me.hasPermission('SEND_MESSAGES'))return;
+            if(!message.guild.me.permissions.has('SEND_MESSAGES'))return;
             const cmd = client.commands.find((c) => c.name.toLowerCase() == command || c.alias.includes(command));
             if (cmd) {
-                if (!message.member.hasPermission(cmd.userPermissions) && cmd.userPermissions.length > 0) return message.inlineReply("No cuentas con permisos suficientes");
-                if (!message.guild.me.hasPermission(cmd.botPermissions) && cmd.botPermissions.length > 0) return message.inlineReply(`No cuento con permisos suficientes, nesesito: \`${cmd.botPermissions.join('` `')}\``);
+                if (!message.member.permissions.has(cmd.userPermissions) && cmd.userPermissions.length > 0) return message.inlineReply("No cuentas con permisos suficientes");
+                if (!message.guild.me.permissions.has(cmd.botPermissions) && cmd.botPermissions.length > 0) return message.inlineReply(`No cuento con permisos suficientes, nesesito: \`${cmd.botPermissions.join('` `')}\``);
                 cmd.run(client, message, args);
             }
         } catch (error) {
