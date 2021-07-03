286496bf61e7d98c2ab00bc39e538347b34fe92b
Elí Maciel <tecno_eli@hotmail.com>
2021-07-03 12:52:46 -0500

    util.replace arreglado

--- js/modules/replace.js
+++ js/modules/replace.js
@@ -1,12 +1,15 @@
 module.exports = async (text, array = []) => {
     try {
         if(!text) return new Error('Requiere el parametro "text"');
+        // for (const i of array) {
+        //     console.log();
+        // }
         await array.forEach((i, j) => {
             if(!i.match) return new Error(`Error en: array[${j}], match no encontrado`)
             if(!i.replace) return new Error(`Error en: array[${j}], replace no encontrado`)
             if(!(i.match.constructor === String || i.match.constructor === RegExp)) return new Error(`Error en: array[${j}].match, valor no valido, requiere RegExp o String`)
             if(!(i.replace.constructor === String || i.replace.constructor === Number)) return new Error(`Error en: array[${j}].replace, valor no valido, requiere String o Number`);
-            text = text.replace("{prefix}", i.replace);
+            text = text.replace(i.match, i.replace);
         });
         return text;
     } catch (error) {
--- js/modules/uno.js
+++ js/modules/uno.js
@@ -5,13 +5,13 @@ module.exports.cartas = require('./uno/cartas');
 module.exports.comer = require('./uno/comer');
 module.exports.repartir = (partida) => {
     return new Promise(async (resolve, reject) => {
-        let cartas = ['0r', '1r', '2r', '3r', '4r', '5r', '6r', '7r', '8r', '9r', '1r', '2r', '3r', '4r', '5r', '6r', '7r', '8r', '9r', '0y', '1y', '2y', '3y', '4y', '5y', '6y', '7y', '8y', '9y', '1y', '2y', '3y', '4y', '5y', '6y', '7y', '8y', '9y', '0b', '1b', '2b', '3b', '4b', '5b', '6b', '7b', '8b', '9b', '1b', '2b', '3b', '4b', '5b', '6b', '7b', '8b', '9b', '0g', '1g', '2g', '3g', '4g', '5g', '6g', '7g', '8g', '9g', '1g', '2g', '3g', '4g', '5g', '6g', '7g', '8g', '9g', 'py', 'pb', 'pg', 'pr', 'py', 'pb', 'pg', 'pr', 'cy', 'cb', 'cg', 'cr', 'cy', 'cb', 'cg', 'cr', 'ry', 'rb', 'rr', 'rg', 'ry', 'rb', 'rr', 'rg', 'cc', 'cc', 'cc', 'cc', 'cb', 'cr', 'cg', 'cy', 'cb', 'cr', 'cg', 'cy'];
+        let cartas = ['0r', '1r', '2r', '3r', '4r', '5r', '6r', '7r', '8r', '9r', '0y', '1y', '2y', '3y', '4y', '5y', '6y', '7y', '8y', '9y', '0b', '1b', '2b', '3b', '4b', '5b', '6b', '7b', '8b', '9b', '0g', '1g', '2g', '3g', '4g', '5g', '6g', '7g', '8g', '9g', 'py', 'pb', 'pg', 'pr', 'cy', 'cb', 'cg', 'cr', 'ry', 'rb', 'rr', 'rg', 'cc', 'cb', 'cr', 'cg', 'cy'];
         for (const j of partida.jugadores) {
             partida[j] = {
                 cartas: []
             };
-            for (i = 7; i; i--) partida[j].cartas.push(cartas.splice(cartas.indexOf(cartas[Math.floor(Math.random() * cartas.length)]), 1)[0]);
-            partida.jugadas = [cartas.splice(cartas.indexOf(cartas[Math.floor(Math.random() * cartas.length)]), 1)[0]];
+            for (i = 7; i; i--) partida[j].cartas.push(cartas[Math.floor(Math.random() * cartas.length)]);
+            partida.jugadas = cartas[Math.floor(Math.random() * cartas.length)];
         }
         partida.cartas = cartas;
         resolve(partida);
