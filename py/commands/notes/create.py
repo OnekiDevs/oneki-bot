039fdd8303fe37464a3b4a05b9b5068a6d2916a8
Gael <gapa151107@gmail.com>
2021-07-09 21:04:23 -0500

    Embed de notes al no invocar un subcomando

--- py/commands/notes/create.py
+++ py/commands/notes/create.py
@@ -1,5 +1,5 @@
 from commands.notes import index_subcommand as index
 
 @index.notes.command()
-async def create(ctx):
+async def create(ctx, notebook, description):
     pass
--- py/commands/notes/index_subcommand.py
+++ py/commands/notes/index_subcommand.py
@@ -4,5 +4,14 @@ import tools
 async def notes(ctx):
     #translations = tools.utils.translations(tools.get_config(ctx), "commands/notes")
     if (ctx.invoked_subcommand is None):
-
-        await ctx.send("Comando en construcción jaja")
+        collection = tools.db.ctx("notes")
+        embed = tools.discord.Embed(
+            description = "Aqui un listado de tus cuadernos", 
+            colour = ctx.author.color
+        )
+        embed.set_author(name = f"Notebooks", icon_url = ctx.author.avatar_url)
+        embed.set_thumbnail(url = "https://media.discordapp.net/attachments/725140299873124372/863238505109913630/depositphotos_325926388-stock-illustration-blue-notebook-with-spiral-vector.png")
+        notebooks = collection.get(f"{ctx.author.id}")
+        for i in notebooks:
+            embed.add_field(name = f"{i}", value = f"```{notebooks[i]}```", inline = False)
+        await ctx.send(embed = embed)
