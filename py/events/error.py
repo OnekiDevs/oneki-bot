88fcd87caf46491bc13aa6d698b6645d8cf0c18c
Gael <gapa151107@gmail.com>
2021-07-10 19:28:08 -0500

    Mejoras en el evento error, ahora tiene mas contexto

--- py/events/error.py
+++ py/events/error.py
@@ -1,29 +1,35 @@
 import tools 
+import traceback
 
 @tools.bot.event
 async def on_command_error(ctx, error):
     try: 
         translations = tools.utils.translations(tools.get_config(ctx), "events/error")
     except(tools.exceptions.WrongPrefix): return
-    if(type(error) == tools.commands.errors.MissingPermissions):
+    if(isinstance(error, tools.commands.errors.MissingPermissions)):
         msg = translations["MissingPermissions"]
         for i in error.missing_perms:
             msg = msg + f"`{i}`\n"
         await ctx.send(msg)
-    elif(type(error) == tools.commands.errors.BotMissingPermissions):
+    elif(isinstance(error, tools.commands.errors.BotMissingPermissions)):
         msg = translations["BotMissingPermissions"]
         for i in error.missing_perms:
             msg = msg + f"`{i}`\n"
         await ctx.send(msg)
-    elif(type(error) == tools.commands.errors.MissingRequiredArgument): 
+    elif(isinstance(error, tools.commands.errors.MissingRequiredArgument)): 
         await ctx.send(translations["MissingRequiredArgument"].format(error.param.name))
-    elif(type(error) == tools.commands.errors.CommandNotFound): pass
-    elif(type(error) == tools.exceptions.WrongPrefix): pass
+    elif(isinstance(error, tools.commands.errors.CommandNotFound)): pass
+    elif(isinstance(error, tools.exceptions.WrongPrefix)): pass
     else: 
         channel = tools.bot.get_channel(833780614712131616)
-        embed = tools.discord.Embed(description = "Se a producido un error D:", color = tools.discord.Colour.blue())
-        embed.set_author(name = "Link Error", url = f"{ctx.message.jump_url}")
-        embed.add_field(name = "Tipo:", value = f"{type(error)}")
-        embed.add_field(name = "Detalle:", value = f"{error}", inline = False)
+        msg = "".join(traceback.format_exception(type(error), error, error.__traceback__))
+        embed = tools.discord.Embed(description = "Más datos del error", color = tools.discord.Colour.blue())
+        embed.set_author(name = "Error", url = f"{ctx.message.jump_url}")
+        embed.add_field(name = "Tipo:", value = f"```{type(error)}```", inline = False)
+        embed.add_field(name = "Mensaje:", value = f"```{ctx.message.content}```")
+        embed.add_field(name = "Detalle:", value = f"```{error}```", inline = False)
         embed.set_image(url = "https://cdn.discordapp.com/attachments/725140299873124372/855196781632290846/error.gif")
-        await channel.send(embed = embed)
+        msg = "".join(traceback.format_exception(type(error), error, error.__traceback__))
+        await channel.send("**Contexto:**\n```py\n" + msg + "\n```", embed = embed)
+        print('Ignoring exception in command {}:'.format(ctx.command))
+        traceback.print_exception(type(error), error, error.__traceback__)
