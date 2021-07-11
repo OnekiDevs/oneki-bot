e3b286659a9dbd0acdf1f78dc335453df6a8b429
Gael <gapa151107@gmail.com>
2021-07-10 19:28:49 -0500

    Subcomando create creado y funcionando

--- py/commands/notes/create.py
+++ py/commands/notes/create.py
@@ -1,5 +1,13 @@
 from commands.notes import index_subcommand as index
 
 @index.notes.command()
-async def create(ctx, notebook, description):
-    pass
+async def create(ctx, notebook, *, description = "Sin descripción"):
+    #translations = tools.utils.translations(tools.get_config(ctx), "commands/notes")
+    async with ctx.typing():
+        collection = index.tools.db.ctx("notes")
+        document = collection.get(f"{ctx.author.id}")
+        if document is not None:
+            collection.update(f"{ctx.author.id}", notebook, description)
+        else:
+            collection.set(f"{ctx.author.id}", {notebook : description})
+    await ctx.send("Se creo el cuaderno **{}** correctamente!".format(notebook))
