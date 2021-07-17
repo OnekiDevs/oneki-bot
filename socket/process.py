6e8a8990f19e8c18212c691cbbcd204998fabca8
Gael <gapa151107@gmail.com>
2021-07-16 20:57:34 -0500

    Pequeños cambios al socket

--- socket/process.py
+++ socket/process.py
@@ -16,18 +16,18 @@ def data_to_all(clients : list, data, client):
 def Process(clients : list):
     while True:
         if (len(clients) > 0):
-            for c in clients:
+            for client in clients:
                 try:
-                    data = c.recv(1024)
+                    data = client.recv(1024)
                     if data:
                         d = pickle.loads(data)
                         print(d)
                         if isinstance(d, dict):
                             if d["event"] in name_events: 
-                                data_to_all(clients, data, c)
+                                data_to_all(clients, data, client)
                             else: 
-                                c.send(pickle.dumps("Invalid event"))
+                                client.send(pickle.dumps("Invalid event"))
                         else: 
-                            c.send(pickle.dumps("Invalid data type"))
+                            client.send(pickle.dumps("Invalid data type"))
                 except:
                     pass
