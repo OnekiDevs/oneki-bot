148d42061b024634e363fde54e814ae1902978a0
Elí Maciel <tecno_eli@hotmail.com>
2021-07-27 00:49:23 -0500

    /config language set agregado

--- js/slash/config/language.js
+++ js/slash/config/language.js
@@ -1,9 +1,29 @@
 const db = require('firebase-admin').firestore();
 module.exports = {
     set: async (client, interact, options) => {
-        
+        console.log(options);
+        const lang = options.get('language');
+        db.collection('config').doc(interact.guildId).update({
+            lang: lang.value
+        }).catch((err) => {
+            if(err.details.startsWith("No document to update")) db.collection('config').doc(interact.guildId).set({
+                lang: lang.value
+            })
+        });
+        interact.reply({
+            content: `Idioma cambiado a \`${lang.name}\``
+        })
     },
     reset: (client, interact, options) => {
-        
+        db.collection('config').doc(interact.guildId).update({
+            lang: 'en'
+        }).catch((err) => {
+            if(err.details.startsWith("No document to update")) db.collection('config').doc(interact.guildId).set({
+                lang: 'en'
+            })
+        });
+        interact.reply({
+            content: `Idioma cambiado a \`english\``
+        })
     }
 }
