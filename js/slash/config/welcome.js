3a0d5d48aa9f25d05fc948234cd59caf27ca956b
Elí Maciel <tecno_eli@hotmail.com>
2021-07-27 22:57:05 -0500

    /config wolcome cambiado
    se cambio el comando `/config welcome set` a `/config welcome channel` y `/config welcome delete` a `/config welcome desactivate` y se creo `/config welcome rols`

--- js/slash/config/welcome.js
+++ js/slash/config/welcome.js
@@ -1,9 +1,8 @@
 const db = require('firebase-admin').firestore();
-// const d = require('firebase-admn').firestore
+const FieldValue = require('firebase-admin').firestore.FieldValue;
 module.exports = {
-    set: async (client, interact, options) => {
+    channel: async (client, interact, options) => {
         const channel = options.get('channel');
-        console.log(channel.channel.guild.id);
         db.collection(channel.channel.guild.id).doc('bienvenidas').update({ 
             channel: channel.value 
         }).catch(err => {
@@ -15,10 +14,36 @@ module.exports = {
             content: `Bienvenidas establecido en \`${channel.channel.name}\``
         })
     }, 
-    delete: async (client, interact, options) => { 
-        db.collection(channel.channel.guild.id).doc('bienvenidas').delete()
+    deactivate: async (client, interact, options) => {
+        const func = options.get('function');
+        if (func.value == 'welcome') {
+            db.collection(interact.guildId).doc('bienvenidas').update({ 
+                channel: FieldValue.delete()
+            })
+            interact.reply({
+                content: `Bienvenidas eliminadas`
+            })
+        } else {
+            db.collection(interact.guildId).doc('bienvenidas').update({ 
+                roles: FieldValue.delete()
+            })
+            interact.reply({
+                content: `Roles eliminados`
+            })
+        }
+    }, 
+    rols: async (client, interact, options) => {
+        const rol = options.get('rol');
+        db.collection(rol.role.guild.id).doc('bienvenidas').update({ 
+            roles: FieldValue.arrayUnion(rol.value)
+        }).catch(err => {
+            if (err.details.startsWith("No document to update")) db.collection(rol.role.guild.id).doc('bienvenidas').set({
+                roles: [rol.value]
+            })
+        })
         interact.reply({
-            content: `Bienvenidas eliminadas`
+            content: `Rol \`${rol.role.name}\` agregado`,
+            ephemeral: true
         })
     }
 }
