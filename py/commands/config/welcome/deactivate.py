4c2def6bc0c578828a924c0365930de2f93e1ead
Gael <gapa151107@gmail.com>
2021-07-27 19:48:19 -0500

    subcomandos welcome adaptados al nuevo modulo db
    Ahora se usa el nuevo modulo de db y uno que otro cambio mas

--- py/commands/config/welcome/deactivate.py
+++ py/commands/config/welcome/deactivate.py
@@ -5,13 +5,23 @@ from commands.config.welcome import index_subcommand as index
 async def deactivate(ctx, etc = None):
     translations = index.tools.utils.translations(index.tools.get_config(ctx), "commands/config/welcome")
     message = translations["deactivate"]["defecto"]
+
     async with ctx.typing():
+        document = index.tools.db.Document(collection = f"{ctx.guild.id}", document = "bienvenidas")
         if(etc == "roles"):
-            index.tools.db.ctx(f"{ctx.guild.id}").delete("bienvenidas", "roles")
+            # index.tools.db.ctx(f"{ctx.guild.id}").delete("bienvenidas", "roles")
+            document.delete("roles")
             message = translations["deactivate"]["roles"]
+
         elif(etc == "message"):
-            index.tools.db.ctx(f"{ctx.guild.id}").delete("bienvenidas", "channel")
-            index.tools.db.ctx(f"{ctx.guild.id}").delete("bienvenidas", "message")
+            # index.tools.db.ctx(f"{ctx.guild.id}").delete("bienvenidas", "channel")
+            # index.tools.db.ctx(f"{ctx.guild.id}").delete("bienvenidas", "message")
+            document.delete("channel")
+            document.delete("message")
             message = translations["deactivate"]["message"]
-        else: index.tools.db.ctx(f"{ctx.guild.id}").delete("bienvenidas")
+
+        else: 
+            # index.tools.db.ctx(f"{ctx.guild.id}").delete("bienvenidas")
+            document.delete()
+
     await ctx.send(message)
