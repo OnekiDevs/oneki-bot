3c81958080dbb9b040e6a61ab2563a722807dc6e
Gael <gapa151107@gmail.com>
2021-07-27 19:48:36 -0500

    Update lang.py
    Ahora usa el nuevo modulo db

--- py/commands/config/lang.py
+++ py/commands/config/lang.py
@@ -7,17 +7,27 @@ idioms = ["en", "es", "fr", "zh", "ko", "ru"]
 async def lang(ctx, language):
     translations = tools.utils.translations(tools.get_config(ctx), "commands/config/lang")
     try: 
+        idioms.index(language)
         async with ctx.typing():
-            idioms.index(language)
-            collection = tools.db.ctx("config")
-            server = collection.get(f"{ctx.guild.id}")
-            if(server == None): collection.set(f"{ctx.guild.id}", {"lang" : language})
+            document = tools.db.Document(collection = "config", document = f"{ctx.guild.id}")
+
+            if document.exists:
+                tools.servers[f"{ctx.guild.id}"]["lang"] = language
+                document.update("lang", language)
             else: 
-                try: 
-                    tools.servers[f"{ctx.guild.id}"]["prefix"] = server["prefix"]
-                except(KeyError): 
-                    tools.servers[f"{ctx.guild.id}"]["prefix"] = ">"
-                collection.update(f"{ctx.guild.id}", "lang", language)
-            tools.servers[f"{ctx.guild.id}"]["lang"] = language
+                tools.servers[f"{ctx.guild.id}"] = {"prefix": ">", "lang": language}
+                document.set(lang = language, prefix = ">")
+
+            # if(server == None): collection.set(f"{ctx.guild.id}", {"lang" : language})
+            # else: 
+            #     try: 
+            #         tools.servers[f"{ctx.guild.id}"]["prefix"] = server["prefix"]
+            #     except(KeyError): 
+            #         tools.servers[f"{ctx.guild.id}"]["prefix"] = ">"
+            #     collection.update(f"{ctx.guild.id}", "lang", language)
+            # tools.servers[f"{ctx.guild.id}"]["lang"] = language
+
         await ctx.send(translations["msg"])
-    except(ValueError): await ctx.send(translations["msg_error"])
+
+    except(ValueError): 
+        await ctx.send(translations["msg_error"])
