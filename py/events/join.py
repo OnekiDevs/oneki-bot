f779995c0a857377c08d628c7770ac6de4309fac
Gael <gapa151107@gmail.com>
2021-07-29 00:17:33 -0500

    cambios en los eventos
    Ahora usan el nuevo modulo db y ahora el code esta mas limpio y entendible (alto desmadre era antes xd)

--- py/events/join.py
+++ py/events/join.py
@@ -1,37 +1,55 @@
 import tools
+from tools.utils import roles
 
-async def img(msg, mensaje): 
-    img = tools.Image.open("src/welcome.png")
+async def img(guild: tools.discord.Guild, user: tools.discord.User, mensaje): 
+    # Imagen welcome
+    welcome_img = tools.Image.open("src/welcome.png")
     font = tools.ImageFont.truetype("src/arial.ttf", 30)
-    tools.ImageDraw.ImageDraw(img).text((210, 155), text = mensaje.format(msg.name, msg.guild.name), font = font, fill = "white")
+    tools.ImageDraw.ImageDraw(welcome_img).text( 
+        xy = (210, 155), 
+        font = font,
+        fill = "white",
+        text = mensaje.format(user.name, guild.name)
+    )
 
-    pfp = tools.Image.open(tools.BytesIO(await msg.avatar_url_as(format = "png", size = 128).read()))
+    # Pfp del usuario
+    pfp = tools.Image.open(tools.BytesIO(await user.avatar_url_as(format = "png", size = 128).read()))
     pfp = pfp.resize((100, 100))
     bigsize = (pfp.size[0] * 3, pfp.size[1] * 3)
     mask = tools.Image.new('L', bigsize, 0)
+
     tools.ImageDraw.ImageDraw(mask).ellipse((0, 0) + bigsize, fill=255)
     mask = mask.resize(pfp.size, tools.Image.ANTIALIAS)
     pfp.putalpha(mask)
+
     output = tools.ImageOps.fit(pfp, mask.size, centering = (0.5, 0.5))
     output.putalpha(mask)
     output.save('src/pfp.png')
 
-    img.paste(pfp, (89, 121), pfp)
-    img.save("src/cache.png")
+    welcome_img.paste(pfp, (89, 121), pfp)
+    welcome_img.save("src/cache.png")
+
+    return tools.discord.File(
+        fp = "src/cache.png", 
+        filename = "welcome.png",
+        # spoilers = True
+    )
 
 @tools.bot.event
-async def on_member_join(message):
-    collection = tools.db.ctx(f"{message.guild.id}")
-    data = collection.get("bienvenidas")
-    if(data == None): pass
-    else:
-        roles = collection.get("bienvenidas", "roles")
-        if(roles is not None and tools.utils.is_empty(roles) == False):
-            await tools.utils.give_list_roles(message.guild, message, roles)
-        try:
-            channel = tools.bot.get_channel(int(data["channel"]))
-            await img(message, data["message"])
-            await channel.send("{0.mention}".format(message), file = tools.discord.File("src/cache.png", filename = "welcome.png"))
+async def on_member_join(member):
+    document = tools.db.Document(collection = f"{member.guild.id}", document = "bienvenidas")
+    if document.exists:
+        content = document.content
+        list_roles = content.get("roles")
+        if (list_roles is not None) and (tools.utils.is_empty(list_roles) == False):
+            await roles.give_list_roles(member.guild, member, list_roles)
+
+        channel_id = content.get("channel")
+        if channel_id is not None:
+            channel = tools.bot.get_channel(int(channel_id))
+
+            welcome_img = await img(member.guild, member, content.get("message"))
+            await channel.send("{0.mention}".format(member), file = welcome_img)
+
             tools.remove("src/cache.png")
             tools.remove("src/pfp.png")
-        except(KeyError): pass
--- py/events/messages.py
+++ py/events/messages.py
@@ -3,14 +3,17 @@ import tools
 @tools.bot.event
 async def on_message(message):
     await tools.bot.process_commands(message)
-    if(message.author.bot): return
 
-    if(message.content.split(' ')[0] == "<@!832137212266283049>"):
-        try:
-            server = tools.servers[f"{message.guild.id}"]
+    if message.author.bot: 
+        return
+
+    # si pingearon al bot
+    if tools.bot.user.mentioned_in(message):
+        server = tools.servers.get(f"{message.guild.id}")
+        if server is not None:
             prefix = server["prefix"]
             translations = tools.utils.translations(server["lang"], "events/message")
-        except(KeyError): 
+        else:
             prefix = ">"
             translations = tools.utils.translations("en", "events/message")
         await message.channel.send(translations["ping"].format(prefix))
