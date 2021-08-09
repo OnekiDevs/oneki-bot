65fc94363f4a98c2c882d66d3d17b758f5396094
Gael <gaelporraz@gmail.com>
2021-08-08 22:11:57 -0500

    Cambios en el socket y ajustes en el comando afk

--- py/tools/client.py
+++ py/tools/client.py
@@ -2,22 +2,25 @@ import socket
 import pickle
 import threading
 
-def send(sock, name_event, server : str, data : str):
-	sock.send(pickle.dumps({"event" : name_event, "data" : {"server" : str(server), "value" : str(data)}}))
+def send(sock, **kwargs):
+	sock.send(pickle.dumps({
+		"event" : kwargs.get("event"), 
+		"server" : kwargs.get("server"),  
+		"new_v" : kwargs.get("value"), 
+	}))
 
-def __recv(sock, servers):
+def _recv(sock, servers):
 	while True:
-			data = sock.recv(1024)
-			if data:
-				d = pickle.loads(data)
-				event = d["event"]
-				servers[d["data"]["server"]][event] = d["data"]["value"] 
+		data = sock.recv(1024)
+		if data:
+			data = pickle.loads(data)
+			servers[ data[ "server" ] ][ data["event"] ] = data[ "new_v" ] 
 
 def connect(servers, host: str = "localhost", port: int = 4000) -> socket.socket:
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.connect((str(host), int(port)))
 
-	recv = threading.Thread(target = lambda : __recv(sock, servers))
+	recv = threading.Thread(target = lambda: _recv(sock, servers))
 	recv.daemon = True
 	recv.start()
 
