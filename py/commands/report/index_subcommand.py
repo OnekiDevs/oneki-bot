853cf7f942513034cae9bd374b8d3b5cb69f810f
Gael <gaelporraz@gmail.com>
2021-08-11 20:49:51 -0500

    Comando report terminado (tal vez aun existan bugs)

--- py/commands/report/index_subcommand.py
+++
@@ -1,8 +0,0 @@
-import tools
-from tools.utils import commands
-
-@tools.bot.group()
-async def report(ctx):
-    translations = tools.translations(commands.get_config(ctx), "commands/report")
-    if (ctx.invoked_subcommand is None):
-        await ctx.send("Comando en construcción jaja")
