1259da8981a7d5b2f4ccf5b294bd06793c0640e4
Elí Maciel <tecno_eli@hotmail.com>
2021-08-16 22:39:35 -0500

    manejo de errores en comandos

--- js/events/command.js
+++ js/events/command.js
@@ -1,24 +1,49 @@
-const { Permissions, MessageEmbed } = require('discord.js');
+const { Permissions, MessageEmbed } = require("discord.js");
 module.exports = {
-    name: 'command',
+    name: "command",
     run: async (client, command, message, args) => {
-        if(!message.guild.me.permissions.has(Permissions.FLAGS.SEND_MESSAGES)) return;
+        if (!message.guild.me.permissions.has(Permissions.FLAGS.SEND_MESSAGES)) return;
         const cmd = client.commands.find((c) => c.name.toLowerCase() == command || c.alias.includes(command));
         if (cmd) {
             const server = client.servers.get(message.guild.id);
-            const lang = client.util.lang({lang:server.lang, route:'events/command'});
-            if (cmd.userPermissions?.length > 0 && !message.member.permissions.has(cmd.userPermissions)) return; 
-            if (cmd.botPermissions?.length > 0 && !message.guild.me.permissions.has(cmd.botPermissions)) return message.reply(await client.util.replace(lang.botPermissions, [{match:"{permissions}", replace:`\`${new Permissions(cmd.botPermissions).toArray().join('` `')}\``}]));
-            try {
-                cmd.run(client, message, args);
-            } catch (error) {
-                console.log('\x1b[31m%s\x1b[0m', '**********************************************************************');
-                console.error(error);
-                console.log('\x1b[31m%s\x1b[0m', '**********************************************************************');
-                client.channels.cache.get('833780614712131616').send({
-                    embeds: [new MessageEmbed().setColor('YELLOW').setTitle('New Error Detected')]
-                })
-            }
+
+            const lang = client.util.lang({ lang: server.lang, route: "events/command" });
+            if (cmd.userPermissions?.length > 0 && !message.member.permissions.has(cmd.userPermissions)) return;
+            if (cmd.botPermissions?.length > 0 && !message.guild.me.permissions.has(cmd.botPermissions))
+                return message.reply(
+                    await client.util.replace(lang.botPermissions, [
+                        {
+                            match: "{permissions}",
+                            replace: `\`${new Permissions(cmd.botPermissions).toArray().join("` `")}\``,
+                        },
+                    ])
+                );
+            cmd.run(client, message, args).catch(async (error) => {
+                console.log(
+                    "\x1b[31m%s\x1b[0m",
+                    "**********************************************************************"
+                );
+                console.log(error);
+                console.log(
+                    "\x1b[31m%s\x1b[0m",
+                    "**********************************************************************"
+                );
+                (await client.channels.fetch("833780614712131616")).send({
+                    content: process.env.NODE_ENV!='production'?process.env.DEVELOPER_ID?`<@${process.env.DEVELOPER_ID}>`:null:'<@&832657759081463848>',
+                    embeds: [
+                        new MessageEmbed()
+                            .setColor("YELLOW")
+                            .setTitle("New Error Detected")
+                            .addField("Error Type", "```cmd\n" + error.name + "\n```", true)
+                            .addField("Error Message", "```cmd\n" + error.message + "\n```", true)
+                            .addField("Error In", `\`\`\`cmd\ncommand ${cmd.name}\n\`\`\``, true),
+                        new MessageEmbed()
+                            .setColor("YELLOW")
+                            .setTitle("Error Stack")
+                            .setDescription(`\`\`\`cmd\n${error.stack}\n\`\`\``),
+                    ],
+                });
+            });
         }
-    }
-}
+    },
+};
