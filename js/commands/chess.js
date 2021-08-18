b91c75b824a1ea4027284470ad0f94c56feb840b
Elí Maciel <tecno_eli@hotmail.com>
2021-08-17 23:10:04 -0500

    >chess agregado

---
+++ js/commands/chess.js
@@ -0,0 +1,45 @@
+const fetch = require("node-fetch");
+const { MessageButton, MessageActionRow } = require("discord.js");
+const shortid = require("shortid");
+module.exports = {
+    name: "chess",
+    botPermissions: [],
+    userPermissions: [],
+    alias: [],
+    run: async (client, message, args) => {
+        const server = client.servers.get(message.guild.id);
+        const lang = client.util.lang({lang:server.lang, route:'commands/chess'});
+        const channel = message.mentions.channels.find((m) => m.type == "GUILD_VOICE");
+        if (!channel) return message.reply(lang.mention);
+        const invite = await channel.createInvite({
+            targetApplication: "832012774040141894",
+            targetType: 2,
+        });
+        const ID = shortid.generate();
+        message.reply({
+            content: `${await client.util.replace(lang.message, [
+                { match: "{user}", replace: message.member.displayName },
+            ])}`,
+            components: [
+                new MessageActionRow().addComponents([
+                    new MessageButton()
+                        .setLabel(lang.accept)
+                        .setStyle("LINK")
+                        .setURL(`https://discord.com/invite/${invite.code}`),
+                    new MessageButton().setLabel(lang.invitation).setStyle("PRIMARY").setCustomId(ID),
+                ]),
+            ],
+        });
+        client.buttons.set(ID, {
+            params: {
+                url: `https://discord.com/invite/${invite.code}`,
+            },
+            run: (client, interact, { url }) => {
+                interact.reply({
+                    content: url,
+                    ephemeral: true,
+                });
+            },
+        });
+    },
+};
