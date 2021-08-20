5f6c6cbef8493098767d2eeaf54347b75cc517c8
Elí Maciel <tecno_eli@hotmail.com>
2021-08-20 00:50:37 -0500

    menciones agregadas a >ss

--- js/commands/ss.js
+++ js/commands/ss.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const  { MessageAttachment, Permissions } = require('discord.js')
+// const {URLSearchParams} = require('url')
 module.exports = {
     name: 'ss',
     botPermissions: [Permissions.FLAGS.MANAGE_MESSAGES],
@@ -22,27 +23,40 @@ module.exports = {
             }
         });
         // console.log(args[0], args[0].match(/<@!?(\d{17,19})>/))
-        let params = "text="+(!args[0].match(/<@!?(\d{17,19})>/)?args.join(" "):args.slice(1).join(" "));
+        let params = "text="+(!args[0].match(/<@!?(\d{17,19})>/)?args.join(" "):args.slice(1).join(" ")), mentions = {}
+        // const params = new URLSearchParams().set('text',(!args[0].match(/<@!?(\d{17,19})>/)?args.join(" "):args.slice(1).join(" ")))
+        message.mentions.members.map(m=>{
+            // params.set(m.id, m.displayName)
+            mentions[m.id] = m.displayName
+        })
+        if(Object.keys(mentions).length > 0) params += "&mentions=" + JSON.stringify(mentions)
         if (args[0].match(/<@!?(\d{17,19})>/) && message.mentions.members.first()) {
+            // params.set('user', message.mentions.members.first()?.displayName)
+            // params.set('avatar', message.mentions.members.first()?.displayAvatarURL())
+            // params.set('color', message.mentions.members.first()?.displayHexColor.slice(1))
+            // params.set('bot', message.mentions.users.first()?.bot?'1':'0')
+            // params.set('verified', message.mentions.users.first()?.flags.has('VERIFIED_BOT')?'1':'0')
             params += `&user=${message.mentions.members.first()?.displayName}`;
             params += `&avatar=${message.mentions.users.first()?.displayAvatarURL()}`;
             params += `&color=${message.mentions.members.first()?.displayHexColor.slice(1)}`
             params += `&bot=${message.mentions.users.first()?.bot?'1':'0'}`
             params += `&verified=${message.mentions.users.first()?.flags.has('VERIFIED_BOT')?'1':'0'}`
         } else {
+            // params.set('user', message.member.displayName)
+            // params.set('avatar', message.author.displayAvatarURL())
+            // params.set('color', message.member.displayHexColor.slice(1))
+            // params.set('bot', message.author.bot?'1':'0')
+            // params.set('verified', message.author.flags.has('VERIFIED_BOT')?'1':'0')
             params += `&user=${message.member.displayName}`;
             params += `&avatar=${message.author.displayAvatarURL()}`;
             params += `&color=${message.member.displayHexColor.slice(1)}`
             params += `&bot=${message.author.bot?'1':'0'}`
             params += `&verified=${message.author.flags.has('VERIFIED_BOT')?'1':'0'}`
         }
-        const page = await browser.newPage();
-        await page.goto(`http://oneki.herokuapp.com/api/fakeDiscordMessage?${params}`);
-        const ss = await page.screenshot();
-        const attachment = new MessageAttachment(ss);
-        // console.log(attachment);
+        const page = await browser.newPage()
+        await page.goto(`http://localhost:3000/api/fakeDiscordMessage?${params}`);
         message.channel.send({
-            files: [attachment]
+            files: [new MessageAttachment(await page.screenshot())]
         });
         await browser.close();
     }
