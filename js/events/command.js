47d6f8f2c7f57848d2b154824336c90549c3148a
Elí Maciel <tecno_eli@hotmail.com>
2021-08-21 01:05:21 -0500

    example.env

--- js/events/command.js
+++ js/events/command.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "command",
     run: async (client, command, message, args) => {
         if (!message.guild.me.permissions.has(Permissions.FLAGS.SEND_MESSAGES)) return;
-        if (client.servers.get(message.guild.id).blacklist.channels.includes(message.channel.id)) return;
+        if (client.servers.get(message.guild.id).blacklist.channels.includes(message.channel.id)) return;   
         const cmd = client.commands.find((c) => c.name.toLowerCase() == command || c.alias.includes(command));
         if (cmd) {
             const server = client.servers.get(message.guild.id);
