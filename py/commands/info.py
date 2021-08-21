0adec6ae79f352eb26aca481145f92ce7ebb064b
Gael <gaelporraz@gmail.com>
2021-08-20 19:14:15 -0500

    Cambios en las traducciones de info

--- py/commands/info.py
+++ py/commands/info.py
@@ -23,15 +23,15 @@ async def info(ctx, member: tools.discord.Member = None):
 
     if(member.activity != None): 
         if isinstance(member.activity, tools.discord.CustomActivity):
-            embed.add_field(name = translations["embed"]["field_1"]["name"], value = f"```{member.activity}```", inline = False)
+            embed.add_field(name = translations["embed"]["field_1"], value = f"```{member.activity}```", inline = False)
         else:
-            embed.add_field(name = translations["embed"]["field_1"]["name"], value = f"```{member.activity.name}```", inline = False)
+            embed.add_field(name = translations["embed"]["field_1"], value = f"```{member.activity.name}```", inline = False)
 
-    embed.add_field(name = translations["embed"]["field_2"]["name"], value = f"```{user.created_at}```")
-    embed.add_field(name = translations["embed"]["field_3"]["name"], value = f"```{member.joined_at}```")
-    embed.add_field(name = translations["embed"]["field_4"]["name"], value = f"```{member.color}```")
-    embed.add_field(name = translations["embed"]["field_5"]["name"], value = f"```{user.id}```")
-    embed.add_field(name = translations["embed"]["field_6"]["name"], value = f"```{member.raw_status}```")
+    embed.add_field(name = translations["embed"]["field_2"], value = f"```{user.created_at}```")
+    embed.add_field(name = translations["embed"]["field_3"], value = f"```{member.joined_at}```")
+    embed.add_field(name = translations["embed"]["field_4"], value = f"```{member.color}```")
+    embed.add_field(name = translations["embed"]["field_5"], value = f"```{user.id}```")
+    embed.add_field(name = translations["embed"]["field_6"], value = f"```{member.raw_status}```")
     embed.set_footer(icon_url = ctx.author.avatar_url, text = translations["embed"]["footer"].format(ctx.author.name))
 
     await ctx.send(embed = embed)
