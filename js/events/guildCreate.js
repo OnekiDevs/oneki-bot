a82607ba944eed47c504096e71b08c266dbe2903
Elí Maciel <tecno_eli@hotmail.com>
2021-08-23 23:11:41 -0500

    Update guildCreate.js

--- js/events/guildCreate.js
+++ js/events/guildCreate.js
@@ -4,15 +4,15 @@ module.exports = {
     run: async (client, guild) => {
         let canal = client.channels.cache.get("867248825777127445");
         const embed = new MessageEmbed()
-            .setThumbnail(guild.iconURL)
+            .setThumbnail(guild.iconURL())
             .setTitle("Me añadieron en un Nuevo Servidor")
             .setDescription(`ahora estoy en ${client.guilds.cache.size} servidores`)
             .addField("Servidor", guild.name, true)
             .addField("ID", guild.id, true)
-            .addField("Region", guild.region, true)
+            // .addField("Region", guild.region, true)
             .addField("Roles", guild.roles.cache.size, true)
             .addField("Miembros", guild.memberCount, true)
-            .addField("Dueño", `${guild.owner.user.username}#${guild.owner.user.discriminator}`, true)
+            .addField("Dueño", `${(await client.users.fetch(guild.ownerId)).username}#${(await client.users.fetch(guild.ownerId)).discriminator}`, true)
             .setTimestamp()
             .setColor("RANDOM")
             .setFooter(`${client.user.username} Bot ${require('../../package.json').version}`)
