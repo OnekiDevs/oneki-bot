873a9cb5455c4f54931f2495c24d21882b45451c
Elí Maciel <tecno_eli@hotmail.com>
2021-08-23 21:09:49 -0500

    manejo de errores en interacciones

--- js/events/interactionCreate.js
+++ js/events/interactionCreate.js
@@ -1,21 +1,48 @@
 module.exports = {
     name: 'interactionCreate',
     run: async (client, interact) => {
-        // console.log(interact);
-        if(interact.isButton()) {
-            if (interact.customId.startsWith('help_')) {
-                let cmd = client.buttons.get('help');
-                cmd.run(client, interact, {
-                    category: interact.customId.split('_')[2],
-                    lang: interact.customId.split('_')[1]
-                })
-            } else {
-                let cmd = client.buttons.get(interact.customId);
-                if (cmd) cmd.run(client, interact, cmd.params)
+        try {
+            // console.log(interact);
+            if(interact.isButton()) {
+                if (interact.customId.startsWith('help_')) {
+                    let cmd = client.buttons.get('help');
+                    cmd.run(client, interact, {
+                        category: interact.customId.split('_')[2],
+                        lang: interact.customId.split('_')[1]
+                    })
+                } else {
+                    let cmd = client.buttons.get(interact.customId);
+                    if (cmd) cmd.run(client, interact, cmd.params)
+                }
+            } else if (interact.isCommand()){
+                let cmd = client.slash.get(interact.commandName)
+                if (cmd) cmd.run(client, interact)
             }
-        } else if (interact.isCommand()){
-            let cmd = client.slash.get(interact.commandName)
-            if (cmd) cmd.run(client, interact)
+        } catch (error) {
+            console.log(
+                "\x1b[31m%s\x1b[0m",
+                "**********************************************************************"
+            );
+            console.log(error);
+            console.log(
+                "\x1b[31m%s\x1b[0m",
+                "**********************************************************************"
+            );
+            (await client.channels.fetch("833780614712131616")).send({
+                content: process.env.NODE_ENV!='production'?process.env.DEVELOPER_ID?`<@${process.env.DEVELOPER_ID}>`:null:'<@&832657759081463848>',
+                embeds: [
+                    new MessageEmbed()
+                        .setColor("YELLOW")
+                        .setTitle("New Error Detected")
+                        .addField("Error Type", "```cmd\n" + error.name + "\n```", true)
+                        .addField("Error Message", "```cmd\n" + error.message + "\n```", true)
+                        .addField("Error In", `\`\`\`cmd\n${interact.isButton()?`button ${interact.customId}`:interact.isCommand()?`command /${interact.name}`:'event interactionCreate'}\n\`\`\``, true),
+                    new MessageEmbed()
+                        .setColor("YELLOW")
+                        .setTitle("Error Stack")
+                        .setDescription(`\`\`\`cmd\n${error.stack}\n\`\`\``),
+                ],
+            });
         }
     }
 }
