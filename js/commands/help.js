2c0cb2f92cd9ce7e0b5226d8068e4f15ade9b711
Elí Maciel <tecno_eli@hotmail.com>
2021-09-16 17:34:19 -0500

    0.13.3
    `>help` arreglado

--- js/commands/help.js
+++ js/commands/help.js
@@ -9,34 +9,21 @@ module.exports = {
         message.channel.sendTyping();
         const server = client.servers.get(message.guild.id);
         fetch(`https://oneki.herokuapp.com/api/lang/${server.lang}/cmd/categories`).then(async r => {
-            const body = await r.text();
-            console.log(body)
             try {
-                return JSON.parse(body);
-            } catch (err) {
-                console.error("Error:", err);
-                console.error("Response body:", body);
-                // throw err;
+                return JSON.parse(await r.text());
+            } catch (_) {
                 return {
                     categories: []
                 }
             }
         }).then((categories) => {
-            categories = categories.categories;
-            console.log(categories)
             fetch(`https://oneki.herokuapp.com/api/lang/${server.lang}/cmd/${categories[0]}`).then(async (r) => {
-                const body = await r.text();
-                console.log(body)
                 try {
-                    return JSON.parse(body);
-                } catch (err) {
-                    console.error("Error:", err);
-                    console.error("Response body:", body);
-                    // throw err;
+                    return JSON.parse(await r.text());
+                } catch (_) {
                     return []
                 }
             }).then(async category=>{
-                console.log(category)
                 const lang = client.util.lang({lang:server.lang, route:'commands/help'}),
                     embed = new MessageEmbed().setTitle(`${await client.util.replace(lang.embed.title, [{match:"{bot}", replace:message.guild.me.displayName}])}`).setDescription(`${await client.util.replace(lang.embed.description, [{match:"{type}", replace:categories[0]}])}`).setColor('#f89dfa');
                 let j = 0, k = 0, buttons = [];
