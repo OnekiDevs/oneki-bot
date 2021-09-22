2e0176b0d752e5782340d0e2f2fa9a0059504d9a
Gael <69738055+Gael-devv@users.noreply.github.com>
2021-09-21 20:06:12 -0500

    quien toco esta chingadera? :hrr:

--- py/commands/config/channel/report.py
+++ py/commands/config/channel/report.py
@@ -14,9 +14,4 @@ async def report(ctx, channel: tools.discord.TextChannel):
         else:
             document.set(channel = f"{channel.id}")
 
-        if document.exist:
-            document.update("channel", f"{channel.id}") 
-        else: 
-            document.set(channel = f"{channel.id}") 
-
     await ctx.send(translations['channel_config'])
