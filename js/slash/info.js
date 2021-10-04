a7b8d16a7ec48e11d362575a6937643613d8c29a
Elí Maciel <tecno_eli@hotmail.com>
2021-10-04 00:47:57 -0500

    `/info member`

--- js/slash/info.js
+++ js/slash/info.js
@@ -25,9 +25,7 @@ module.exports = {
      * @returns {Promise<void>}
      */
     run: async (interact) => {
-        interact.deferReply({
-            ephemeral: true
-        })
+        interact.deferReply()
         await util.sleep()
         if(interact.options.getSubcommand() === 'member') {
             const member = interact.options.getMember('member')??interact.member
@@ -45,19 +43,35 @@ module.exports = {
                     Authorization: `Bot ${TOKEN}`
                 }
             })).json()
-            // member.fetch()
-            console.log(fetcuser);
-            // const banner = (await member.user.fetch())
-            // console.log(banner)
+            console.log(fetcuser)
             const embed = new MessageEmbed()
                 .setTitle(`${member.displayName} info`)
+                .setDescription(`${member.user.bot?`Es Bot${member.user.flags.has('VERIFIED_BOT')?' verificado':''}`:''}
+                ${member.pending?'Miembro pendiente de verificación':''}
+                ${member.user.flags.has('HOUSE_BALANCE')?'House Balance':''}
+                ${member.user.flags.has('HOUSE_BRILLIANCE')?'House Brilliance':''}
+                ${member.user.flags.has('HOUSE_BRAVERY')?'House Bravery':''}
+                ${member.user.flags.has('DISCORD_EMPLOYEE')?'Empleado de Discord':''}
+                ${member.user.flags.has('DISCORD_CERTIFIED_MODERATOR')?'Moderador Certificado de Discord':''}
+                ${member.user.flags.has('HYPESQUAD_EVENTS')?'Eventos de HypeSquad':''}
+                ${member.user.flags.has('BUGHUNTER_LEVEL_1')?'Cazador de Bugs Nivel 1':''}
+                ${member.user.flags.has('BUGHUNTER_LEVEL_2')?'Cazador de Bugs Nivel 2':''}
+                ${member.user.flags.has('EARLY_VERIFIED_BOT_DEVELOPER')?'Desarrollador de Bots Verificado':''}`)
                 .addField('ID', member.id, true)
                 .addField('Tag', member.user.tag, true)
-                .setThumbnail(fetcmember.avatar?`https://cdn.discordapp.com/guilds/${member.guild.id}/users/${member.id}/avatars/${fetcmember.avatar}.webp?size=1024`:member.user.displayAvatarURL())
-                .setImage(fetcuser.banner?`https://cdn.discordapp.com/banners/${member.id}/${fetcuser.banner}.webp?size=1024`:'')
-
+                .addField('Color', `${member.displayColor} / ${member.displayHexColor}`, true)
+                .addField('Fecha  de creación', `<t:${member.user.createdTimestamp}:d>`, true)
+                .addField('Entro el', `<t:${member.joinedTimestamp}:d> <t:${member.joinedTimestamp}:R>`, true)
+                .setColor(member.displayColor)
+                .setThumbnail(fetcmember.avatar?`https://cdn.discordapp.com/guilds/${member.guild.id}/users/${member.id}/avatars/${fetcmember.avatar}.png?size=512`:member.user.displayAvatarURL({size:512}))
+                .setImage(fetcuser.banner?`https://cdn.discordapp.com/banners/${member.id}/${fetcuser.banner}.${fetcuser.banner.startsWith('a_')?'gif':'png'}?size=1024`:'')
+            if(member.premiumSinceTimestamp) embed.addField('Boosteando desde hace', `<t:${member.premiumSinceTimestamp}:R>`, true)
+            if(member.user)
+            embed.addField('Roles', member.roles.cache.map(r=>`${r}`).join(' '))
+            let embeds = [embed, new MessageEmbed().setTitle('Avatar').setURL(member.user.displayAvatarURL({dynamic:true,size:2048})).setImage(member.user.displayAvatarURL({dynamic:true,size:1024})).setColor(member.displayColor)]
+            if (fetcmember.avatar) embeds.push(new MessageEmbed().setTitle('Avatar en server').setURL(`https://cdn.discordapp.com/guilds/${member.guild.id}/users/${member.id}/avatars/${fetcmember.avatar}.${fetcmember.avatar.startsWith('a_')?'gif':'png'}?size=2048`).setImage(`https://cdn.discordapp.com/guilds/${member.guild.id}/users/${member.id}/avatars/${fetcmember.avatar}.${fetcmember.avatar.startsWith('a_')?'gif':'png'}?size=1024`).setColor(member.displayColor))
             interact.editReply({
-                embeds: [embed]
+                embeds: embeds
             })
         }
     }
