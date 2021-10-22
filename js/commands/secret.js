19b7221e994077b69059d4a455a0f5faf60e783e
Elí Maciel <tecno_eli@hotmail.com>
2021-10-22 13:29:52 -0500

    Update secret.js

--- js/commands/secret.js
+++ js/commands/secret.js
@@ -17,8 +17,15 @@ module.exports = class Ping extends require('../classes/Command'){
         if(!DEVELOPERS.includes(message.author.id)) return;
         if(!args[0]) return;
         switch (args.shift()) {
+            case 'prueba':
+                message.reply('esta')
+                break;
             case 'say':
-                message.channel.send(args.join(' '))
+                if((/<#d{18}>).test(args[0])) {
+                    const ch = message.mentions.channels.cache.first();
+                    args.shift()
+                    ch.send(args.join(' '))
+                } else message.channel.send(args.join(' '))
                 break;
             default:
                 message.reply('shh')
