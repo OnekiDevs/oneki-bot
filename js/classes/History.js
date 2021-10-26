c1013ecd16b5fc04ad1977b11d78b2abfe5799d1
Elí Maciel <tecno_eli@hotmail.com>
2021-10-25 22:26:17 -0500

    clase `History` creada

---
+++ js/classes/History.js
@@ -0,0 +1,45 @@
+const Queue = require('./Queue')
+const EventEmitter = require('node:events')
+const HistoryManager = require('./HistoryManager')
+const FieldValue = require('firebase-admin').firestore.FieldValue;
+module.exports = class History extends EventEmitter {
+
+    cache = new HistoryManager()
+    last = 1
+    guildId = null
+
+    constructor(guildId) {
+        super()
+
+        this.guildId = guildId
+
+        db.collection(guildId).doc('voice').get().then(doc => {
+            if(doc.exists && doc.data().last) this.last = doc.data().last
+        })
+
+        this.on('historyCreated', QueueItem => {
+            this.cache.add(QueueItem)
+            db.collection(QueueItem.guildId).doc('voice').update({
+                musicHistory: FieldValue.arrayUnion({
+                    link: QueueItem.link
+                })
+            }).catch(err => {
+                if (err.details.startsWith("No document to update")) db.collection(channel.guild.id).doc("deleted").set({
+                    musicHistory: [{
+                        link: QueueItem.link
+                    }]
+                });
+            })
+        })
+    }
+
+    add(QueueItem) {
+        QueueItem.reproducedTimestamp = new Date().now()
+        this.emit('historyCreated', QueueItem)
+    }
+
+    getHistory() {
+        return this.cache.map(e=>({link:e.link}));
+    }
+
+}
