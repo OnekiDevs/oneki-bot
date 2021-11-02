cac3f0b922867e8dd13be051b074fbdfa3197b23
Elí Maciel <tecno_eli@hotmail.com>
2021-11-01 21:51:46 -0600

    Update messageDelete.js

--- js/events/messageDelete.js
+++ js/events/messageDelete.js
@@ -16,16 +16,17 @@ module.exports = {
                 .setTitle(lang.embed.title[message.type]??lang.embed.fields.author.DEFAULT)
                 .setURL(message.url)
                 .setColor("RANDOM")
-                .setDescription('```\n'+message.content+'\n```')
                 .setAuthor(message.author.username, message.author.displayAvatarURL())
                 .addField(lang.embed.fields.deletedIn[message.type]??lang.embed.fields.deletedIn.DEFAULT, `${message.channel} | ${message.channel.name}`, true)
                 .setTimestamp()
                 .setThumbnail(message.author.displayAvatarURL({dynamic: true}))
                 .addField(lang.embed.fields.write, `<t:${Math.round(message.createdTimestamp/1000)}>`, true)
                 .setFooter(`${client.user.username} Bot ${require('../../package.json').version}`, client.user.avatarURL());
+            if(message.content) embed.setDescription('```\n'+message.content+'\n```')
             canal.send({
                 content: message.author.id,
-                embeds: [embed]
+                embeds: [embed],
+                stickers: message.stickers.map(s=>s)
             });
         } catch (e) {
             util.error(e, __filename)
