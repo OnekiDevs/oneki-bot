913d872d82830ab79a85f993c226c3ec6df44e74
Elí Maciel <tecno_eli@hotmail.com>
2021-11-30 22:57:05 -0600

    error handler on /config

--- js/slash/config.js
+++ js/slash/config.js
@@ -99,9 +99,13 @@ module.exports = {
     },
     servers: [],
     run: async (interact) => {
-        require(`./config/${interact.options.getSubcommandGroup()}`)[interact.options.getSubcommand()](
-            interact,
-            interact.options
-        );
+        try {
+            require(`./config/${interact.options.getSubcommandGroup()}`)[interact.options.getSubcommand()](
+                interact,
+                interact.options
+            );
+        } catch (e) {
+            throw e
+        }
     },
 };
