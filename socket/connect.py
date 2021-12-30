a47d24cb615704c0dca87af67827dca358c98ed7
Gael <69738055+Gael-devv@users.noreply.github.com>
2021-12-29 22:07:46 -0500

    deleted socket

--- socket/connect.py
+++
@@ -1,8 +0,0 @@
-def connect(sock, clients: list):
-    while True:
-        try:
-            conn, addr = sock.accept()
-            conn.setblocking(False)
-            clients.append(conn)
-        except:
-            pass
