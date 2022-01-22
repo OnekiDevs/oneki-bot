70f23a3398137b89baf001f5eb2ca8112126f47b
eliyya <tecno_eli@hotmail.com>
2022-01-22 00:36:42 -0600

    purge files transpiled

--- js/buttons/clear_no.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (interact, params) => {
-    const message = await interact.message.channel.messages.fetch(interact.message.reference.messageId)
-    if (interact.member.id == message.member.id) {
-        interact.message.delete();
-        message.delete();
-    }
-    interact.deferUpdate();
-}
-module.exports.id = 'clear_no';
